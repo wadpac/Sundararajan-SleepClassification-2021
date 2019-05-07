ccb0476ac58d15f89857871cc701631a19f210a0
¨kalaivani¨ <kalaigrad@gmail.com>
2019-05-07 14:48:02 +0200

    Updated

--- deeplearning/mcfly_classification.py
+++ deeplearning/mcfly_classification.py
@@ -8,7 +8,7 @@ from collections import Counter
 
 from sklearn.model_selection import GroupKFold, StratifiedKFold
 
-from metrics import macro_f1
+from metrics import weighted_f1
 from data_augmentation import jitter, time_warp, rotation, rand_sampling
 
 np.random.seed(2)
@@ -159,14 +159,14 @@ def main(argv):
       # Generate candidate architectures
       model = modelgen.generate_models(in_X_train.shape, \
                                     number_of_classes=num_classes, \
-                                    number_of_models=1, metrics=[macro_f1])  
+                                    number_of_models=1, metrics=[weighted_f1])  
 
       # Compare generated architectures on a subset of data for few epochs
       outfile = os.path.join(resultdir, 'model_comparison.json')
       hist, acc, loss = find_architecture.train_models_on_samples(in_X_train, \
                                  in_y_train, in_X_test, in_y_test, model, nr_epochs=5, \
                                  subset_size=in_X_train.shape[0], verbose=True, \
-                                 outputfile=outfile, metric='macro_f1')
+                                 outputfile=outfile, metric='weighted_f1')
       val_acc.append(acc[0])
       models.append(model[0])
 
