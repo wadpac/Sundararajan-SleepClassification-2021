5282036205282bc0cbe3a4e540c7a83f9acb3eb8
¨kalaivani¨ <kalaigrad@gmail.com>
2019-09-15 19:07:16 +0200

    Updated nonwear window

--- preprocessing/get_calib_nonwear.R
+++ preprocessing/get_calib_nonwear.R
@@ -1,10 +1,10 @@
 library(GGIR)
 
-indir <- 'C:/Research/actigraphy/data/amc_new/amc_extracted/wrap/'
+indir <- 'C:/Research/actigraphy/data/amc_new/amc_extracted/ssmd/'
 outdir <- 'C:/Research/actigraphy/data/amc_new/'
 fileType = '*.bin'
 dir.create(outdir, showWarnings = FALSE)
 
 files <- list.files(path = indir, pattern=fileType)
 nfiles <- length(files)
-g.part1(datadir=indir, outputdir=outdir, f0=1, f1=nfiles)
+g.part1(datadir=indir, outputdir=outdir, f0=1, f1=nfiles, windowsizes=c(5,900,900), chunksize=1)
