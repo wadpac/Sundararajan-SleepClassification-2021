70e3dad80dea3a489eaa746a2603c0d6c76e3e49
kalaivani-s <kalaigrad@gmail.com>
2019-09-15 20:02:34 +0200

    Updated

--- deeplearning/data_augmentation.py
+++ deeplearning/data_augmentation.py
@@ -1,136 +1,100 @@
-# Ref: https://nbviewer.jupyter.org/github/terryum/Data-Augmentation-For-Wearable-Sensor-Data/blob/master/Example_DataAugmentation_TimeseriesData.ipynb
-# T. T. Um et al., “Data augmentation of wearable sensor data for parkinson’s disease monitoring 
-# using convolutional neural networks,” in Proceedings of the 19th ACM International Conference 
-# on Multimodal Interaction, ser. ICMI 2017. New York, NY, USA: ACM, 2017, pp. 216–220.
-
+import os
+import random
 import numpy as np
-from scipy.interpolate import CubicSpline      # for warping
+from collections import Counter
+from transforms import jitter, time_warp, rotation, rand_sampling
 
-# X is a LxMxN timeseries signal with L samples, M timesteps and N channels (easier batch processing for L samples)
+# augment data - aug_factor : factor by which majority class samples are augmented.
+# Samples from all other classes will be augmented by same amount
+def augment(X, y, sleep_states, aug_factor=1.0, step_sz = 10000):
+  if not os.path.exists('tmp'):
+    os.makedirs('tmp')
 
-# Jitter - adds additive noise
-def jitter(X, sigma=0.05):
-  noise = np.random.normal(loc=0, scale=sigma, size=X.shape)
-  return X + noise
+  # List of possible variations
+  variant_func = [jitter, time_warp, rotation, rand_sampling]
 
-# Scaling - changes the magnitude of the data in a window by multiplying by a random scalar 
-def scaling(X, sigma=0.1):
-  sc_factor = np.random.normal(loc=1.0, scale=sigma, size=(X.shape[0],1,X.shape[2]))
-  noise = np.matmul(np.ones((X.shape[0],X.shape[1],1)), sc_factor)
-  return X*noise
-  
-# Magnitude warping - changes the magnitude of each sample by convolving 
-# the data window with a smooth curve varying around one 
-def generate_random_curves(X, sigma=0.2, knot=4):
-  random_curves = np.zeros(X.shape)
-  for i in range(X.shape[0]):
-    xx = (np.ones((X.shape[2],1))*(np.arange(0,X.shape[1], (X.shape[1]-1)/(knot+1)))).transpose()
-    yy = np.random.normal(loc=1.0, scale=sigma, size=(knot+2, X.shape[2]))
-    x_range = np.arange(X.shape[1])
-    cs_x = CubicSpline(xx[:,0], yy[:,0])
-    cs_y = CubicSpline(xx[:,1], yy[:,1])
-    cs_z = CubicSpline(xx[:,2], yy[:,2])
-    random_curves[i,:,:] = np.array([cs_x(x_range),cs_y(x_range),cs_z(x_range)]).transpose()
-  return random_curves
+  y_lbl = y.argmax(axis=1)
+  y_lbl = [sleep_states[i] for i in y_lbl]
+  y_ctr = Counter(y_lbl).most_common()
+  print(y_ctr)
 
-def magnitude_warp(X, sigma=0.2):
-    return X * generate_random_curves(X, sigma)
+  # Find max number of samples for each class
+  # Create memory mapped arrays to store data after augmentation
+  max_samp = int(y_ctr[0][1]*aug_factor)
+  naug_samp = max_samp * len(sleep_states)
+  X_aug = np.memmap('tmp/X_aug.np', dtype='float32', mode='w+', shape=(naug_samp,X.shape[1],X.shape[2]))
+  y_aug = np.memmap('tmp/y_aug.np', dtype='int32', mode='w+', shape=(naug_samp,y.shape[1]))
+  for i,state in enumerate(y_ctr):
+    lbl,ctr = state
+    idx = sleep_states.index(lbl)
+    lbl_y = y[y[:,idx] == 1,:]
+    lbl_x = X[y[:,idx] == 1,:]
+    st_idx = i*max_samp
 
-# Time warping - by smoothly distorting the time intervals between samples, 
-# the temporal locations of the samples are changed 
-def distort_timesteps(X, sigma=0.2):
-  tt = generate_random_curves(X, sigma) # Regard these samples around 1 as time intervals
-  tt_cum = np.cumsum(tt, axis=1)        # Add intervals to make a cumulative graph
-  # Make the last value to have X.shape[1]
-  t_scale = np.array([(X.shape[1]-1)/tt_cum[:,-1,0],(X.shape[1]-1)/tt_cum[:,-1,1],(X.shape[1]-1)/tt_cum[:,-1,2]]).transpose()
-  t_scale = np.matmul(np.ones((X.shape[0],X.shape[1],1)),t_scale.reshape(X.shape[0],1,X.shape[2]))
-  tt_cum = tt_cum*t_scale
-  return tt_cum
+    # Augment each class with random variations
+    # Choose a random set of samples and apply a single variation
+    # Toss a coin and choose to apply another variation
+    # Append new samples to augmented data
+    n_aug = max_samp - ctr
+    if n_aug > 0:
+      print('%s: Augmenting %d samples to %d samples' % (lbl,n_aug,ctr))
+      end_idx = st_idx + lbl_x.shape[0]
+      X_aug[st_idx:end_idx,:,:] = lbl_x
+      y_aug[st_idx:end_idx,:] = lbl_y
+      if n_aug <= lbl_x.shape[0]: # Few samples to be augmented
+        rand_idx = np.random.randint(0,lbl_x.shape[0],n_aug)
+        aug_x = random.choice(variant_func)(lbl_x[rand_idx])
+        toss = random.choice([0,1])
+        if toss == 1:
+          aug_x = random.choice(variant_func)(aug_x)
+        st_idx = end_idx
+        end_idx = st_idx + aug_x.shape[0]
+        X_aug[st_idx:end_idx,:,:] = aug_x
+        aug_y = np.zeros((n_aug,len(sleep_states))); aug_y[:,idx] = 1
+        y_aug[st_idx:end_idx,:] = aug_y
+      else: # If more samples to be augmented, create variations in batches of step_sz
+        aug_x = None; aug_y = None
+        for j in range(0,n_aug,step_sz):
+          if j+step_sz < n_aug:
+              sz = step_sz
+          else:
+              sz = n_aug-j
+          rand_idx = np.random.randint(0,lbl_x.shape[0],sz)
+          x_var = random.choice(variant_func)(lbl_x[rand_idx])
+          toss = random.choice([0,1])
+          if toss == 1:
+            x_var = random.choice(variant_func)(x_var)
+          y_var = np.zeros((sz,len(sleep_states))); y_var[:,idx] = 1
+          if aug_x is not None:
+            aug_x = np.vstack((aug_x,x_var))
+            aug_y = np.vstack((aug_y,y_var))
+          else:
+            aug_x = x_var
+            aug_y = y_var
+        st_idx = end_idx
+        end_idx = st_idx + aug_x.shape[0]
+        X_aug[st_idx:end_idx,:,:] = aug_x
+        y_aug[st_idx:end_idx,:] = aug_y
+    else:
+      print('%s: Choosing %d samples of %d samples' % (lbl,max_samp,ctr))
+      end_idx = st_idx + max_samp
+      rand_idx = np.random.randint(0,lbl_x.shape[0],max_samp)
+      X_aug[st_idx:end_idx,:,:] = lbl_x[rand_idx]
+      y_aug[st_idx:end_idx,:] = lbl_y[rand_idx]
 
-def time_warp(X, sigma=0.2):
-  tt_new = distort_timesteps(X, sigma)
-  X_new = np.zeros(X.shape)
-  for i in range(X.shape[0]):
-    X_new[i,:,0] = np.interp(np.arange(X.shape[1]), tt_new[i,:,0], X[i,:,0])
-    X_new[i,:,1] = np.interp(np.arange(X.shape[1]), tt_new[i,:,1], X[i,:,1])
-    X_new[i,:,2] = np.interp(np.arange(X.shape[1]), tt_new[i,:,2], X[i,:,2])
-  return X_new
 
-# Rotation - applying arbitrary rotations to the existing data can be used as
-# a way of simulating different sensor placements
-def get_rotation_matrices(angle, axis='x'):  
-  cos_angle = np.cos(angle)
-  sin_angle = np.sin(angle)
+  # Shuffle indices
+  shuf_idx = np.arange(X_aug.shape[0])
+  np.random.shuffle(shuf_idx)
+  X_aug = X_aug[shuf_idx]
+  y_aug = y_aug[shuf_idx]
 
-  rot_mat = np.zeros((angle.shape[0],3,3))
-  if axis == 'x':
-     rot_mat[:,0,0] = 1
-     rot_mat[:,1,1] = cos_angle[:,0]
-     rot_mat[:,1,2] = -sin_angle[:,0]
-     rot_mat[:,2,1] = sin_angle[:,0]
-     rot_mat[:,2,2] = cos_angle[:,0]
-  elif axis == 'y':
-     rot_mat[:,0,0] = cos_angle[:,0]
-     rot_mat[:,0,2] = -sin_angle[:,0]
-     rot_mat[:,1,1] = 1
-     rot_mat[:,2,0] = sin_angle[:,0]
-     rot_mat[:,2,2] = cos_angle[:,0]
-  elif axis == 'z':
-     rot_mat[:,0,0] = cos_angle[:,0]
-     rot_mat[:,0,1] = -sin_angle[:,0]
-     rot_mat[:,1,0] = sin_angle[:,0]
-     rot_mat[:,1,1] = cos_angle[:,0]
-     rot_mat[:,2,2] = 1
+  y_lbl = y_aug.argmax(axis=1)
+  y_lbl = [sleep_states[i] for i in y_lbl]
+  y_ctr = Counter(y_lbl).most_common()
+  print(y_ctr)
 
-  return rot_mat
+  # Flush and close memmap objects
+  del(X_aug); del(y_aug)
 
-def rotation(X):
-  ang_lim = 5.0*np.pi/180.0 # maximum angle variation is +/- 5 degrees
-  angle = np.random.uniform(low=-ang_lim, high=ang_lim, size=(X.shape[0],X.shape[2]))
-  
-  Rx = get_rotation_matrices(angle, axis='x')
-  Ry = get_rotation_matrices(angle, axis='y')
-  Rz = get_rotation_matrices(angle, axis='z')
-  
-  R = np.matmul(Rz, np.matmul(Ry,Rx))
-                                          
-  return np.matmul(X , R)
-
-# DO NOT USE Permutation for accelerometry data since it may mess up the sequential properties, frequency spectrum and transitions
-## Permutation - randomly perturb the temporal location of within-window events. 
-## To perturb the location of the data in a single window, we first slice the data 
-## into N samelength segments, with N ranging from 1 to 5, and randomly permute 
-## the segments to create a new window
-#def permutation(X, nPerm=4, minSegLength=10):
-#  X_new = np.zeros(X.shape)
-#  idx = np.random.permutation(nPerm)
-#  bWhile = True
-#  while bWhile == True:
-#    segs = np.zeros(nPerm+1, dtype=int)
-#    segs[1:-1] = np.sort(np.random.randint(minSegLength, X.shape[0]-minSegLength, nPerm-1))
-#    segs[-1] = X.shape[0]
-#    if np.min(segs[1:]-segs[0:-1]) > minSegLength:
-#      bWhile = False
-#  pp = 0
-#  for ii in range(nPerm):
-#    x_temp = X[segs[idx[ii]]:segs[idx[ii]+1],:]
-#    X_new[pp:pp+len(x_temp),:] = x_temp
-#    pp += len(x_temp)
-#  return(X_new)
-
-# Random sampling - randomly sample timesteps and interpolate data inbetween - same timesteps for all three axes
-def rand_sample_timesteps(X, nSample=1000):
-  tt = np.zeros((X.shape[0],nSample), dtype=int)
-  tt[:,1:-1] = np.sort(np.random.randint(1,X.shape[1]-1,(X.shape[0],nSample-2)),axis=1)
-  tt[:,-1] = X.shape[1]-1
-  return tt
-
-def rand_sampling(X, low=0.6, high=0.8):
-  nSample = np.random.randint(int(low*X.shape[1]),int(high*X.shape[1]),1)[0]
-  tt = rand_sample_timesteps(X, nSample)
-  X_new = np.zeros(X.shape)
-  for i in range(X.shape[0]):
-    X_new[i,:,0] = np.interp(np.arange(X.shape[1]), tt[i], X[i,tt[i],0])
-    X_new[i,:,1] = np.interp(np.arange(X.shape[1]), tt[i], X[i,tt[i],1])
-    X_new[i,:,2] = np.interp(np.arange(X.shape[1]), tt[i], X[i,tt[i],2])
-  return X_new
+  return naug_samp
