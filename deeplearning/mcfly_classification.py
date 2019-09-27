217f0ee19e78c0d1eb02cb1f9464b689425bfc37
¨kalaivani¨ <kalaigrad@gmail.com>
2019-09-27 13:42:47 +0200

    data generator

--- deeplearning/mcfly_classification.py
+++ deeplearning/mcfly_classification.py
@@ -137,6 +137,7 @@ def main(argv):
     collate_sleep = ['NREM 1', 'NREM 2', 'NREM 3', 'REM']
 
   valid_sleep_states = [state for state in sleep_states if state != 'Wake_ext']
+  num_classes = len(valid_sleep_states) 
 
   if not os.path.exists(outdir):
     os.makedirs(outdir)
@@ -156,48 +157,53 @@ def main(argv):
     labels = ['Sleep' if lbl in collate_sleep else lbl for lbl in labels]
 
   early_stopping = EarlyStopping(monitor='val_macro_f1', mode='max', verbose=1, patience=2)
+
+  seqlen, n_channels = np.load(files[0]).shape
+  batch_size = 32
  
   # Use nested cross-validation based on users
   # Outer CV
   unique_users = list(set(users))
   random.shuffle(unique_users)
-  outer_cv_splits = 5; inner_cv_splits = 5
-  out_fold_nusers = len(unique_users) // outer_cv_splits
-
+  out_cv_splits = 5; in_cv_splits = 5
+  out_fold_nusers = len(unique_users) // out_cv_splits
+  out_n_epochs = 10; in_n_epochs = 1
   predictions = []
   wake_idx = sleep_states.index('Wake')
   wake_ext_idx = sleep_states.index('Wake_ext')
-  for out_fold in range(outer_cv_splits):
+  for out_fold in range(out_cv_splits):
     print('Evaluating fold %d' % (out_fold+1))
     test_users = unique_users[out_fold*out_fold_nusers:(out_fold+1)*out_fold_nusers]
     trainval_users = [user for user in unique_users if user not in test_users] 
     train_users = trainval_users[:int(0.8*len(trainval_users))]
     val_users = trainval_users[len(train_users):]
-    print(len(train_users), len(val_users), len(test_users))
 
     train_fnames, train_labels = get_partition(files, labels, users, train_users, sleep_states, is_train=True)
     val_fnames, val_labels = get_partition(files, labels, users, val_users, sleep_states)
     test_fnames, test_labels = get_partition(files, labels, users, test_users, sleep_states)
     
-    outer_train_gen = DataGenerator(train_fnames, train_labels, valid_sleep_states, partition='outer_train',\
-                                    batch_size=32, seqlen=1500, n_channels=3, n_classes=len(valid_sleep_states),\
-                                    shuffle=True, augment=False, balance=True)
+    out_train_gen = DataGenerator(train_fnames, train_labels, valid_sleep_states, partition='out_train',\
+                                    batch_size=batch_size, seqlen=seqlen, n_channels=n_channels,\
+                                    n_classes=num_classes, shuffle=True, augment=False, balance=True)
     print('Fold {}: Computing mean and standard deviation'.format(out_fold+1))
-    #mean, std = outer_train_gen.fit()
+    #mean, std = out_train_gen.fit()
     mean = None; std = None
-    outer_val_gen = DataGenerator(val_fnames, val_labels, valid_sleep_states, partition='outer_val',\
-                                  batch_size=32, seqlen=1500, n_channels=3, n_classes=len(valid_sleep_states),\
-                                  mean=mean, std=std)
-    outer_test_gen = DataGenerator(test_fnames, test_labels, valid_sleep_states, partition='outer_test',\
-                                   batch_size=32, seqlen=1500, n_channels=3, n_classes=len(valid_sleep_states),\
-                                   mean=mean, std=std)
+    out_val_gen = DataGenerator(val_fnames, val_labels, valid_sleep_states, partition='out_val',\
+                                  batch_size=1, seqlen=seqlen, n_channels=n_channels,\
+                                  n_classes=num_classes, mean=mean, std=std)
+    out_test_gen = DataGenerator(test_fnames, test_labels, valid_sleep_states, partition='out_test',\
+                                   batch_size=1, seqlen=seqlen, n_channels=n_channels,\
+                                   n_classes=num_classes, mean=mean, std=std)
 
+    # Get class weights
+    out_class_wts = class_weight.compute_class_weight('balanced', np.unique(train_labels), train_labels)
+    print(out_class_wts)
 
     # Inner CV
     val_acc = []
     models = []
-    in_fold_nusers = len(trainval_users) // inner_cv_splits
-    for in_fold in range(inner_cv_splits):
+    in_fold_nusers = len(trainval_users) // in_cv_splits
+    for in_fold in range(in_cv_splits):
       in_val_users = trainval_users[in_fold*in_fold_nusers:(in_fold+1)*in_fold_nusers]
       in_train_users = [user for user in trainval_users if user not in in_val_users] 
    
@@ -205,27 +211,26 @@ def main(argv):
                                                        sleep_states, is_train=True)
       in_val_fnames, in_val_labels = get_partition(files, labels, users, in_val_users, sleep_states)
     
-      inner_train_gen = DataGenerator(in_train_fnames, in_train_labels, valid_sleep_states, partition='inner_train',\
-                                    batch_size=32, seqlen=1500, n_channels=3, n_classes=len(valid_sleep_states),\
-                                    shuffle=True, augment=False, balance=True, mean=mean, std=std)
-      inner_val_gen = DataGenerator(in_val_fnames, in_val_labels, valid_sleep_states, partition='inner_val',\
-                                  batch_size=32, seqlen=1500, n_channels=3, n_classes=len(valid_sleep_states),\
-                                  mean=mean, std=std)
+      in_train_gen = DataGenerator(in_train_fnames, in_train_labels, valid_sleep_states, partition='in_train',\
+                                    batch_size=batch_size, seqlen=seqlen, n_channels=n_channels,\
+                                    n_classes=num_classes, shuffle=True, augment=False, balance=True, mean=mean, std=std)
+      in_val_gen = DataGenerator(in_val_fnames, in_val_labels, valid_sleep_states, partition='in_val',\
+                                  batch_size=1, seqlen=seqlen, n_channels=n_channels,\
+                                  n_classes=num_classes, mean=mean, std=std)
       
-      print(data.columns); exit()
       # Generate candidate architectures
-      model = modelgen.generate_models(in_X_train.shape, \
+      model = modelgen.generate_models((None, seqlen, n_channels), \
                                     number_of_classes=num_classes, \
                                     number_of_models=1, metrics=[macro_f1])#, model_type='CNN')  
 
       # Compare generated architectures on a subset of data for few epochs
       outfile = os.path.join(resultdir, 'model_comparison.json')
-      hist, acc, loss = find_architecture.train_models_on_samples(in_X_train, \
-                                 in_y_train, in_X_test, in_y_test, model, nr_epochs=1, class_weight=out_class_wts, \
-                                 subset_size=len(grp_train_indices)//10, verbose=True, batch_size=50, \
-                                 outputfile=outfile, metric='macro_f1')
+      hist, acc, loss = find_architecture.train_models_on_samples(in_train_gen, in_val_gen,
+                                 model, nr_epochs=n_in_epochs, class_weight=out_class_wts, \
+                                 verbose=True, outputfile=outfile, metric='macro_f1')
       val_acc.append(acc[0])
       models.append(model[0])
+      print(data.columns); exit()
 
     # Choose best model and evaluate values on validation data
     print('Evaluating on best model for fold %d'% fold)
@@ -235,26 +240,15 @@ def main(argv):
     print(best_model_type)
     print(best_params)
   
-    nr_epochs = 10
-    ntrain = out_X_train.shape[0]; nval = ntrain//5
-    val_idx = np.random.randint(ntrain, size=nval)
-    train_idx = [i for i in range(out_X_train.shape[0]) if i not in val_idx]
-    trainX = out_X_train[train_idx]; trainY = out_y_train[train_idx]
-    trainX = load_as_memmap('tmp/trainX.np', shape=trainX.shape, dtype=np.float32, val=trainX)
-    trainY = load_as_memmap('tmp/trainY.np', shape=trainY.shape, dtype=np.int32, val=trainY)
-    valX = out_X_train[val_idx]; valY = out_y_train[val_idx]
-    valX = load_as_memmap('tmp/valX.np', shape=valX.shape, dtype=np.float32, val=valX)
-    valY = load_as_memmap('tmp/valY.np', shape=valY.shape, dtype=np.int32, val=valY)
-    
-    limit_mem()
     if best_model_type == 'CNN':
-      best_model = modelgen.generate_CNN_model(trainX.shape, num_classes, filters=best_params['filters'], \
+      best_model = modelgen.generate_CNN_model((None, seqlen, n_channels), num_classes, filters=best_params['filters'], \
                                       fc_hidden_nodes=best_params['fc_hidden_nodes'], \
                                       learning_rate=best_params['learning_rate'], \
                                       regularization_rate=best_params['regularization_rate'], \
                                       metrics=[macro_f1])
     else:
-      best_model = modelgen.generate_DeepConvLSTM_model(trainX.shape, num_classes, filters=best_params['filters'], \
+      best_model = modelgen.generate_DeepConvLSTM_model((None, seqlen, n_channels), num_classes,\
+                                      filters=best_params['filters'], \
                                       lstm_dims=best_params['lstm_dims'], \
                                       learning_rate=best_params['learning_rate'], \
                                       regularization_rate=best_params['regularization_rate'], \
@@ -264,8 +258,9 @@ def main(argv):
     model_checkpt = ModelCheckpoint(os.path.join(resultdir,'best_model_fold'+str(fold)+'.h5'), monitor='val_macro_f1',\
                                                  mode='max', save_best_only=True)
     history = F1scoreHistory()
-    hist = best_model.fit(trainX, trainY, epochs=nr_epochs, batch_size=50, \
-                             validation_data=(valX, valY), class_weight=out_class_wts, callbacks=[early_stopping, model_checkpt])
+    hist = best_model.fit_generator(out_train_gen, epochs=n_out_epochs, \
+                             validation_data=out_val_gen, class_weight=out_class_wts,\
+                             callbacks=[early_stopping, model_checkpt])
 
     # Plot training history
 #    plt.Figure()
@@ -282,7 +277,7 @@ def main(argv):
 ##    best_model.save(os.path.join(resultdir,'best_model_fold'+str(fold)+'.h5'))
 
     # Predict probability on validation data
-    probs = best_model.predict_proba(out_X_test, batch_size=1)
+    probs = best_model.predict_generator(out_test_gen)
     y_pred = probs.argmax(axis=1)
     y_true = out_y_test.argmax(axis=1)
     predictions.append((out_users, y_true, y_pred))
@@ -293,9 +288,6 @@ def main(argv):
     else:
       save_user_report(predictions, valid_sleep_states, os.path.join(resultdir,'fold'+str(fold)+'_deeplearning_multiclass_results.csv'))
   
-    # Flush and close memmap objects
-    del(out_X_train); del(out_y_train)
-
   get_classification_report(predictions, valid_sleep_states)
 
   # Save user report
