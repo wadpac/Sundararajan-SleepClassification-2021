d539dfea2855edbc18a9fe624ea97f3e93b6f642
¨kalaivani¨ <kalaigrad@gmail.com>
2019-09-27 11:54:49 +0200

    added normalization

--- deeplearning/mcfly_datagenerator.py
+++ deeplearning/mcfly_datagenerator.py
@@ -1,12 +1,14 @@
 import numpy as np
 import keras
 import random
+from tqdm import tqdm
 from transforms import jitter, time_warp, rotation, rand_sampling
 
 class DataGenerator(keras.utils.Sequence):
-  def __init__(self, filenames, labels, classes, batch_size=32, seqlen=100, n_channels=3,
-               n_classes=5, shuffle=False, augment=False, balance=False):
+  def __init__(self, filenames, labels, classes, partition=None, batch_size=32, seqlen=100, n_channels=3,
+               n_classes=5, mean=None, std=None, shuffle=False, augment=False, balance=False):
     'Initialization'
+    self.partition = partition
     self.seqlen = seqlen
     self.batch_size = batch_size
     self.labels = labels
@@ -19,45 +21,41 @@ class DataGenerator(keras.utils.Sequence):
     self.aug_factor = 0.0
     self.aug_func = [jitter, time_warp, rotation, rand_sampling]
     self.balance = balance
+    self.mean = mean
+    self.std = std
     self.on_epoch_end()
 
   def __len__(self):
     'Denotes the number of batches per epoch'
-    return int(np.floor(len(self.filenames) / self.batch_size))
+    return int(np.floor(len(self.filenames)*(1.0+self.aug_factor) / self.batch_size))
 
-    def __getitem__(self, index):
-      'Generate one batch of data'
-      # Generate indices of the batch
-      if self.balance == False: # For inference
-        indices = self.indices[index*self.batch_size:(index+1)*self.batch_size]
-      else: # Balance each minibatch to have same number of classes
-        # Get number of indices from each class
-        if self.augment == True:
-          assert self.aug_factor > 0.0
-        aug_factor = 1.0 + self.aug_factor
-        cls_sz = (self.batch_size / aug_factor) // self.n_classes
-        if cls_sz * aug_factor * self.n_classes < self.batch_size:
-          cls_sz += 1
-        # Generate indices with balanced classes
-        indices = []
-        all_indices = np.arange(len(self.filenames))
-        for cls in self.classes:
-          cls_idx = all_indices[self.labels == cls]
-          indices.extend(np.random.choice(cls_idx,cls_sz))          
-        # Choose batch sized indices
-        random.shuffle(indices)
-        indices = indices[:self.batch_size]
+  def __getitem__(self, index):
+    'Generate one batch of data'
+    # Generate indices of the batch
+    if self.balance == False: # For inference
+      indices = self.indices[index*self.batch_size:(index+1)*self.batch_size]
+    else: # Balance each minibatch to have same number of classes
+      # Get number of indices from each class
+      if self.augment == True:
+        assert self.aug_factor > 0.0
+      aug_factor = 1.0 + self.aug_factor
+      cls_sz = (self.batch_size / aug_factor) // self.n_classes
+      if cls_sz * aug_factor * self.n_classes < self.batch_size:
+        cls_sz += 1
+      # Generate indices with balanced classes
+      indices = []
+      all_indices = np.arange(len(self.filenames))
+      for cls in self.classes:
+        cls_idx = all_indices[self.labels == cls]
+        indices.extend(np.random.choice(cls_idx,cls_sz))          
+      # Choose batch sized indices
+      random.shuffle(indices)
+      indices = indices[:self.batch_size]
 
-      # Generate data
-      X, y = self.__data_generation(indices)
+    # Generate data
+    X, y = self.__data_generation(indices)
 
-      return X, y
-
-  def on_epoch_end(self):
-    'Updates indexes after each epoch'
-    self.indices = np.arange(len(self.filenames))
-    if self.shuffle == True:
-      np.random.shuffle(self.indices)
+    return X, y
 
   def __data_generation(self, indices):
     'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
@@ -87,4 +85,29 @@ class DataGenerator(keras.utils.Sequence):
         aug_x = random.choice(variant_func)(aug_x)
       X[N:,] = aug_x
 
+    # Normalize data if mean and std are present
+    if self.mean is not None and self.std is not None:
+      X = (X - self.mean)/self.std
+
     return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+  
+  def on_epoch_end(self):
+    'Updates indexes after each epoch'
+    self.indices = np.arange(len(self.filenames))
+    if self.shuffle == True:
+      np.random.shuffle(self.indices)
+
+  def fit(self):
+    'Get mean and std deviation for training data'
+    assert 'train' in self.partition
+    mean = np.empty((self.seqlen, self.n_channels))
+    std = np.empty((self.seqlen, self.n_channels))
+    N = len(self.filenames)
+    for i in tqdm(range(N)):
+      mean += np.load(self.filenames[i])
+    self.mean = mean/N
+    for i in tqdm(range(N)):
+      x = np.load(self.filenames[i])
+      std += (x - mean)**2
+    self.std = std/N
+    return self.mean, self.std
