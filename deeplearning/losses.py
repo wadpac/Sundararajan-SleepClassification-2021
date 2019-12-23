d6084f7768c31c452eae939fc5031187a7315061
kalaivani-s <kalaigrad@gmail.com>
2019-12-24 00:40:24 +0100

    added weights to focal loss

--- deeplearning/losses.py
+++ deeplearning/losses.py
@@ -19,20 +19,22 @@ def weighted_categorical_crossentropy(weights):
   return loss
 
   
-def focal_loss(gamma=2.0, alpha=1.0):
+def focal_loss(weights, gamma=2.0, alpha=1.0):
   """
   Focal loss for unbalanced datasets
 
   Variables:
+    weights: class weights
     gamma: focus parameter to emphasize hard examples
     alpha: numpy array of shape (num_classes,) denoting class weights
   """
+  weights = K.variable(weights)
   alpha = K.variable(alpha)
   epsilon = K.epsilon()
 
   def loss(y_true, y_pred):
     y_pred = K.clip(y_pred, epsilon, 1.0-epsilon)
-    ce_loss = y_true * K.log(y_pred)
+    ce_loss = y_true * K.log(y_pred) * weights
     focal_loss = K.pow(1 - y_pred, gamma) * ce_loss * alpha
     focal_loss = -K.sum(focal_loss, axis=-1)
     return focal_loss 
