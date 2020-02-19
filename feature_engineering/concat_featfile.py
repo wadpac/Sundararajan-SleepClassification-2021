34cd3baafcc9dd1d79146a1d7f61e0d632500c90
kalaivani-s <kalaigrad@gmail.com>
2020-02-19 22:39:00 +0100

    Fixed index bug

--- feature_engineering/concat_featfile.py
+++ feature_engineering/concat_featfile.py
@@ -3,14 +3,17 @@ import sys
 import pandas as pd
 
 def main(argv):
-    infile1 = argv[0]
-    infile2 = argv[1]
-    outfile = argv[2]
-    
-    df1 = pd.read_csv(infile1)
-    df2 = pd.read_csv(infile2)
-    out_df = pd.concat((df1,df2), axis=0)
-    out_df.to_csv(outfile, sep=',', index=False)    
+  infile1 = argv[0]
+  infile2 = argv[1]
+  infile3 = argv[2]
+  outfile = argv[3]
+  
+  df1 = pd.read_csv(infile1)
+  df2 = pd.read_csv(infile2)
+  df3 = pd.read_csv(infile3)
+  out_df = pd.concat((df1,df2), axis=0)
+  out_df = pd.concat((out_df,df3), axis=0)
+  out_df.to_csv(outfile, sep=',', index=False)    
     
 if __name__ == "__main__":
     main(sys.argv[1:])
