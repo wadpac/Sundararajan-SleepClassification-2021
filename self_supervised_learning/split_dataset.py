9b7534e95cf5cd09d75bf64d9c53d83bd4ca836f
kalaivani-s <kalaigrad@gmail.com>
2020-05-01 14:28:35 +0200

    Modified for h5 format

--- self_supervised_learning/split_dataset.py
+++ self_supervised_learning/split_dataset.py
@@ -1,76 +1,68 @@
 import sys,os
 import numpy as np
 import random
+import h5py
+from tqdm import tqdm
+from collections import Counter
+
+def save_partition(infile, indices, partition, outdir, batchsize=1000):
+  with h5py.File(infile,'r') as fp:
+    samples1 = fp['samp1']
+    samples2 = fp['samp2']
+    labels = fp['label']
+    [num_samples, seqlen, channels] = samples1.shape
+    
+    # Save partition
+    num_indices = len(indices)
+    num_batches = num_indices // batchsize
+    if num_batches % batchsize:
+      num_batches += 1
+    for i in tqdm(range(num_batches)):
+      batch_indices = indices[i*batchsize:min(num_indices,(i+1)*batchsize)]  
+      if i == 0:
+        with h5py.File(os.path.join(outdir, partition+'_dataset.h5'),'w') as fp:
+          fp.create_dataset('samp1', data=samples1[batch_indices], chunks=True,\
+                            compression='gzip', maxshape=(None,seqlen,channels))
+          fp.create_dataset('samp2', data=samples2[batch_indices], chunks=True,\
+                            compression='gzip', maxshape=(None,seqlen,channels))
+          fp.create_dataset('label', data=labels[batch_indices], chunks=True,\
+                            compression='gzip', maxshape=(None,))
+      else:
+        with h5py.File(os.path.join(outdir, partition+'_dataset.h5'),'a') as fp:
+          fp['samp1'].resize((fp['samp1'].shape[0] + len(batch_indices)), axis=0)
+          fp['samp1'][-len(batch_indices):] = samples1[batch_indices]
+          fp['samp2'].resize((fp['samp2'].shape[0] + len(batch_indices)), axis=0)
+          fp['samp2'][-len(batch_indices):] = samples2[batch_indices]
+          fp['label'].resize((fp['label'].shape[0] + len(batch_indices)), axis=0)
+          fp['label'][-len(batch_indices):] = labels[batch_indices]
 
 def main(args):
-  sampfile1 = args[0]
-  sampfile2 = args[1]
-  lblfile = args[2]
-  val_perc = float(args[3])
-  test_perc = float(args[4])
-  outdir = args[5]
+  infile = args[0]
+  val_perc = float(args[1])
+  test_perc = float(args[2])
+  outdir = args[3]
 
-  shape_str = sampfile1.split('.npy')[0].split('_')[1]
-  [num_samples, seqlen, channels] = [int(elem) for elem in shape_str.split('x')]
+  with h5py.File(infile,'r') as fp:
+    samples1 = fp['samp1']
+    samples2 = fp['samp2']
+    labels = fp['label']
+    [num_samples, seqlen, channels] = samples1.shape
 
-  num_val = int(val_perc/100.0 * num_samples)
-  num_test = int(test_perc/100.0 * num_samples)
-  num_train = num_samples - num_val - num_test
+    num_val = int(val_perc/100.0 * num_samples)
+    num_test = int(test_perc/100.0 * num_samples)
+    num_train = num_samples - num_val - num_test
 
-  indices = np.arange(num_samples)
-  random.shuffle(indices)
-  train_indices = indices[:num_train]
-  val_indices = indices[num_train:num_train+num_val]
-  test_indices = indices[num_train+num_val:]
+    indices = np.arange(num_samples)
+    random.shuffle(indices)
+    train_indices = np.sort(indices[:num_train])
+    val_indices = np.sort(indices[num_train:num_train+num_val])
+    test_indices = np.sort(indices[num_train+num_val:])
 
-  samples1 = np.memmap(sampfile1, dtype=np.float32, mode='r', shape=(num_samples, seqlen, channels))  
-  samples2 = np.memmap(sampfile2, dtype=np.float32, mode='r', shape=(num_samples, seqlen, channels))  
-  labels = np.memmap(lblfile, dtype=np.int32, mode='r', shape=(num_samples,))  
-
-  # Get train data
-  train_str = 'x'.join(elem for elem in [str(num_train), str(seqlen), str(channels)])
-  train_samples1 = np.memmap(os.path.join(outdir, 'train_samples1_'+train_str+'.npy'), dtype=np.float32, mode='w+',
-                             shape=(num_train, seqlen, channels))
-  train_samples2 = np.memmap(os.path.join(outdir, 'train_samples2_'+train_str+'.npy'), dtype=np.float32, mode='w+',
-                             shape=(num_train, seqlen, channels))
-  train_labels = np.memmap(os.path.join(outdir, 'train_labels_'+train_str+'.npy'), dtype=np.int32, mode='w+',
-                             shape=(num_train,))
-  train_samples1[:,:,:] = samples1[train_indices,:,:]
-  train_samples2[:,:,:] = samples2[train_indices,:,:]
-  train_labels[:] = labels[train_indices]
-  del(train_samples1)
-  del(train_samples2)
-  del(train_labels)
-
-  # Get validation data
-  val_str = 'x'.join(elem for elem in [str(num_val), str(seqlen), str(channels)])
-  val_samples1 = np.memmap(os.path.join(outdir, 'val_samples1_'+val_str+'.npy'), dtype=np.float32, mode='w+',
-                             shape=(num_val, seqlen, channels))
-  val_samples2 = np.memmap(os.path.join(outdir, 'val_samples2_'+val_str+'.npy'), dtype=np.float32, mode='w+',
-                             shape=(num_val, seqlen, channels))
-  val_labels = np.memmap(os.path.join(outdir, 'val_labels_'+val_str+'.npy'), dtype=np.int32, mode='w+',
-                             shape=(num_val,))
-  val_samples1[:,:,:] = samples1[val_indices,:,:]
-  val_samples2[:,:,:] = samples2[val_indices,:,:]
-  val_labels[:] = labels[val_indices]
-  del(val_samples1)
-  del(val_samples2)
-  del(val_labels)
-
-  # Get test data
-  test_str = 'x'.join(elem for elem in [str(num_test), str(seqlen), str(channels)])
-  test_samples1 = np.memmap(os.path.join(outdir, 'test_samples1_'+test_str+'.npy'), dtype=np.float32, mode='w+',
-                             shape=(num_test, seqlen, channels))
-  test_samples2 = np.memmap(os.path.join(outdir, 'test_samples2_'+test_str+'.npy'), dtype=np.float32, mode='w+',
-                             shape=(num_test, seqlen, channels))
-  test_labels = np.memmap(os.path.join(outdir, 'test_labels_'+test_str+'.npy'), dtype=np.int32, mode='w+',
-                             shape=(num_test,))
-  test_samples1[:,:,:] = samples1[test_indices,:,:]
-  test_samples2[:,:,:] = samples2[test_indices,:,:]
-  test_labels[:] = labels[test_indices]
-  del(test_samples1)
-  del(test_samples2)
-  del(test_labels)
+    # Save partitions
+    batchsize = 100
+    save_partition(infile, train_indices, 'train', outdir, batchsize=batchsize)
+    save_partition(infile, val_indices, 'val', outdir, batchsize=batchsize)
+    save_partition(infile, test_indices, 'test', outdir, batchsize=batchsize)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
